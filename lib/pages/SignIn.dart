3bf1cdd28588751884ad0c8985bc86a250a15708
ViolettaNeff <violetta.neff@student.htl-rankweil.at>
2025-06-04 13:15:00 +0200

    Update

--- lib/pages/SignIn.dart
+++ lib/pages/SignIn.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'SignInUser.dart';
+import 'SignInAdmin.dart';
+
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
@@ -6,8 +9,46 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF9F9F9),
       appBar: AppBar(title: const Text("Sign In")),
-      body: const Center(child: Text("Sign In Page")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Sign In Page",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignInAsUserPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+              ),
+              child: const Text('Sign in as User', style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 16),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignInAsAdminPage()),
+                );
+              },
+              style: OutlinedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
+              ),
+              child: const Text('Sign in as Admin'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
