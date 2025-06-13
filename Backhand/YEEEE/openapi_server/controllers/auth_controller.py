88a4f65371eb309c834169da61f57b2c6d02532a
Levi POP <levi.pop@student.htl-rankweil.at>
2025-06-13 11:29:32 +0200

    authentification

--- Backhand/YEEEE/openapi_server/controllers/auth_controller.py
+++ Backhand/YEEEE/openapi_server/controllers/auth_controller.py
@@ -1,57 +1,87 @@
 import connexion
-from typing import Dict
-from typing import Tuple
-from typing import Union
+import bcrypt
+from typing import Dict, Tuple, Union
 
-from openapi_server.models.login import Login  # noqa: E501
-from openapi_server.models.register_admin import RegisterAdmin  # noqa: E501
-from openapi_server.models.register_user import RegisterUser  # noqa: E501
-from openapi_server import util
+from openapi_server.models.login import Login
+from openapi_server.models.register_admin import RegisterAdmin
+from openapi_server.models.register_user import RegisterUser
+from supabase import create_client, Client
 
+# Supabase Zugangsdaten
+SUPABASE_URL = "https://bhgvyhekvowmwirfklih.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoZ3Z5aGVrdm93bXdpcmZrbGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxOTY5NzUsImV4cCI6MjA2NDc3Mjk3NX0.Y5y6FJOSxy2NMRvfRpvTVVloWdxPL0P51cgGVdmz0nU"
 
-def auth_admin_register_post(body):  # noqa: E501
-    """Admin registrieren
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-     # noqa: E501
+# 🔐 Passwort-Hashing
+def hash_password(password: str) -> str:
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
-    :param register_admin: 
-    :type register_admin: dict | bytes
+def verify_password(password: str, hashed: str) -> bool:
+    return bcrypt.checkpw(password.encode("utf-8"), hashed.encode("utf-8"))
 
-    :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
-    """
-    register_admin = body
+# 🧑‍💼 Admin registrieren
+def auth_admin_register_post(body) -> Union[Dict, Tuple[Dict, int]]:
     if connexion.request.is_json:
-        register_admin = RegisterAdmin.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        register_admin = RegisterAdmin.from_dict(connexion.request.get_json())
 
+        existing = supabase.table("Admin").select("*").eq("email", register_admin.email).execute()
+        if existing.data:
+            return {"message": "Admin existiert bereits."}, 409
 
-def auth_login_post(body):  # noqa: E501
-    """Login für Admin/User
+        hashed_pw = hash_password(register_admin.password)
 
-     # noqa: E501
+        supabase.table("Admin").insert({
+            "name": register_admin.name,
+            "email": register_admin.email,
+            "password": hashed_pw
+        }).execute()
 
-    :param login: 
-    :type login: dict | bytes
+        return {"message": "Admin erfolgreich registriert."}, 201
+    return {"message": "Ungültige Eingabe."}, 400
 
-    :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
-    """
-    login = body
+# 👤 User registrieren
+def auth_user_register_post(body) -> Union[Dict, Tuple[Dict, int]]:
     if connexion.request.is_json:
-        login = Login.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        register_user = RegisterUser.from_dict(connexion.request.get_json())
 
+        existing = supabase.table("User").select("*").eq("email", register_user.email).execute()
+        if existing.data:
+            return {"message": "User existiert bereits."}, 409
 
-def auth_user_register_post(body):  # noqa: E501
-    """User registrieren
+        hashed_pw = hash_password(register_user.password)
 
-     # noqa: E501
+        supabase.table("User").insert({
+            "name": register_user.name,
+            "email": register_user.email,
+            "password": hashed_pw
+        }).execute()
 
-    :param register_user: 
-    :type register_user: dict | bytes
+        return {"message": "User erfolgreich registriert."}, 201
+    return {"message": "Ungültige Eingabe."}, 400
 
-    :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
-    """
-    register_user = body
+# 🔑 Login
+def auth_login_post(body) -> Union[Dict, Tuple[Dict, int]]:
     if connexion.request.is_json:
-        register_user = RegisterUser.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        login = Login.from_dict(connexion.request.get_json())
+
+        # Admin prüfen
+        admin = supabase.table("Admin").select("*").eq("email", login.email).execute()
+        if admin.data:
+            admin_data = admin.data[0]
+            if verify_password(login.password, admin_data["password"]):
+                return {"message": "Admin Login erfolgreich."}, 200
+            else:
+                return {"message": "Falsches Passwort."}, 401
+
+        # User prüfen
+        user = supabase.table("User").select("*").eq("email", login.email).execute()
+        if user.data:
+            user_data = user.data[0]
+            if verify_password(login.password, user_data["password"]):
+                return {"message": "User Login erfolgreich."}, 200
+            else:
+                return {"message": "Falsches Passwort."}, 401
+
+        return {"message": "Benutzer nicht gefunden."}, 404
+    return {"message": "Ungültige Eingabe."}, 400
